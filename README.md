17fc23d28d535a41f6d804aa3175f310aa7f9185
Tim Lee <tlee87@gmail.com>
2020-12-18 17:55:03 -0500

    initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # video-cutter
-python lambda for cutting and formatting video
+
+Python lambda for cutting and formatting videos.
+
+# Dependencies
+
+Built with moviepy which uses ffmpeg.
+
+## Basic Settings:
+
+Runtime: Python 3.8
+Memory: 2048 MB
+Timeout: 15min
+
+## Environment vars:
+
+ACCESS_KEY_ID: "XXX"
+CLIP_LENGTH_SECS: 45
+MAX_CLIPS: 5
+SECRET_ACCESS_KEY: "XXX"
+TARGET_BUCKET: "bucket-name"
+TARGET_ROOT_FOLDER: "clips"
+
+## Layers:
+
+AWSLambda-Python38-SciPy1x (v29)
+custom-moviepy-layer
+
+Build custom moviepy layer using the below commands. Will use requirements.txt in the repo to build these dependencies on the aws python3.8 env.
+reference: (https://stackoverflow.com/questions/64016819/cant-use-opencv-python-in-aws-lambda)
+
+terminal1
+$ mkdir /tmp/moviepy-layer && cp requirements.txt /tmp/moviepy-layer/requirements.txt && cd /tmp/moviepy-layer
+
+terminal2
+$ docker run -it -v /tmp/moviepy-layer:/moviepy-layer lambci/lambda:build-python3.8 bash
+$ cd moviepy-layer
+$ pip install -t python/lib/python3.8/site-packages/ -r requirements.txt
+
+## Trigger:
+
+S3 ObjectCreated
+Prefix: originals/
---
+++ lambda_function.py
@@ -0,0 +1,97 @@
+from moviepy.editor import *
+import boto3
+import math
+from os import environ
+from os import listdir
+
+dl_path = '/tmp'
+write_path = '/tmp'
+
+
+def lambda_handler(event, context):
+    data_retrieved = event['Records'][0]['s3']
+    bucket_name = data_retrieved['bucket']['name']
+    file_key = data_retrieved['object']['key']
+    print(file_key)
+    file_paths = file_key.split('/')
+    print(file_paths)
+
+    root_folder = file_paths[0]
+    user_id = file_paths[1]
+    album_id = file_paths[2]
+    full_file_name = file_paths[3]
+
+    if not root_folder or not user_id or not album_id or not full_file_name:
+        return {
+            "statusCode": 422,
+            "headers": {"content-type": "application/json"},
+            "body":  "Invalid folder structure",
+        }
+
+    if 'ACCESS_KEY_ID' in environ:
+        ACCESS_KEY_ID = environ['ACCESS_KEY_ID']
+        SECRET_ACCESS_KEY = environ['SECRET_ACCESS_KEY']
+        TARGET_BUCKET = environ['TARGET_BUCKET']
+        TARGET_ROOT_FOLDER = environ['TARGET_ROOT_FOLDER']
+        CLIP_LENGTH_SECS = int(environ['CLIP_LENGTH_SECS'])
+        MAX_CLIPS = int(environ['MAX_CLIPS'])
+    else:
+        import settings
+        ACCESS_KEY_ID = settings.ACCESS_KEY_ID
+        SECRET_ACCESS_KEY = settings.SECRET_ACCESS_KEY
+        TARGET_BUCKET = settings.TARGET_BUCKET
+        TARGET_ROOT_FOLDER = settings.TARGET_ROOT_FOLDER
+        CLIP_LENGTH_SECS = int(settings.CLIP_LENGTH_SECS)
+        MAX_CLIPS = int(settings.MAX_CLIPS)
+
+    if TARGET_ROOT_FOLDER == root_folder:
+        return {
+            "statusCode": 422,
+            "headers": {"content-type": "application/json"},
+            "body":  "Source and target folders cannot be the same",
+        }
+
+    ext_idx = full_file_name.rfind('.')
+    file_name = full_file_name[0:ext_idx]
+    file_ext = full_file_name[ext_idx+1:]
+    file_dl_path = '{}/{}.{}'.format(dl_path, file_name, file_ext)
+    print(file_dl_path)
+
+    s3 = boto3.client('s3')
+    s3.download_file(
+        bucket_name,
+        file_key,
+        file_dl_path,
+    )
+
+    source_vid = VideoFileClip(file_dl_path)
+    print(source_vid.duration)
+
+    clips = math.ceil(source_vid.duration / CLIP_LENGTH_SECS)
+    if clips > MAX_CLIPS:
+        clips = MAX_CLIPS
+    print('clips {}'.format(clips))
+
+    s3_session = boto3.session.Session(
+        aws_access_key_id=ACCESS_KEY_ID,
+        aws_secret_access_key=SECRET_ACCESS_KEY
+    ).resource('s3')
+
+    for i in range(0, clips):
+        print('processing clip {}'.format(i))
+        suffix = str(i+1)
+        # should clip last video to = video total duration
+        clip = source_vid.subclip(CLIP_LENGTH_SECS*i, CLIP_LENGTH_SECS*(i+1))
+        clip_path = '{}/{}_clip_{}.mp4'.format(write_path, file_name, suffix)
+        clip.write_videofile(clip_path, audio=False)
+        target_key = '{}/{}/{}/{}_clip_{}.mp4'.format(
+            TARGET_ROOT_FOLDER, user_id, album_id, file_name, suffix)
+        print(target_key)
+        s3_session.meta.client.upload_file(
+            clip_path, TARGET_BUCKET, target_key)
+
+    return {
+        "statusCode": 200,
+        "headers": {"content-type": "application/json"},
+        "body":  None,
+    }
