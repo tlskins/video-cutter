a55b3c802a7f68dfb53e575f269b8e5b4d2b86f0
Tim Lee <tlee87@gmail.com>
2021-02-12 16:28:05 -0500

    write clip metas to response and s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 from moviepy.editor import *
 import boto3
 import math
+import json
 from os import environ
 from os import listdir
 
@@ -33,6 +34,7 @@ def lambda_handler(event, context):
         SECRET_ACCESS_KEY = environ['SECRET_ACCESS_KEY']
         TARGET_BUCKET = environ['TARGET_BUCKET']
         TARGET_ROOT_FOLDER = environ['TARGET_ROOT_FOLDER']
+        TARGET_META_FOLDER = environ['TARGET_META_FOLDER']
         CLIP_LENGTH_SECS = int(environ['CLIP_LENGTH_SECS'])
         MAX_CLIPS = int(environ['MAX_CLIPS'])
     else:
@@ -41,6 +43,7 @@ def lambda_handler(event, context):
         SECRET_ACCESS_KEY = settings.SECRET_ACCESS_KEY
         TARGET_BUCKET = settings.TARGET_BUCKET
         TARGET_ROOT_FOLDER = settings.TARGET_ROOT_FOLDER
+        TARGET_META_FOLDER = settings.TARGET_META_FOLDER
         CLIP_LENGTH_SECS = int(settings.CLIP_LENGTH_SECS)
         MAX_CLIPS = int(settings.MAX_CLIPS)
 
@@ -78,24 +81,46 @@ def lambda_handler(event, context):
     ).resource('s3')
 
     outputs = []
-    for i in range(0, clips):
-        print('processing clip {}'.format(i))
-        suffix = str(i+1)
-        # should clip last video to = video total duration
-        clip = source_vid.subclip(CLIP_LENGTH_SECS*i, CLIP_LENGTH_SECS*(i+1))
-        clip_path = '{}/{}_clip_{}.mp4'.format(write_path, file_name, suffix)
+    for clip_num in range(0, clips):
+        print('processing clip {}'.format(clip_num))
+        # clip video
+        startClip = CLIP_LENGTH_SECS*clip_num
+        endClip = CLIP_LENGTH_SECS*(clip_num+1)
+        clip = source_vid.subclip(startClip, endClip)
+        clip_path = '{}/{}_clip_{}.mp4'.format(write_path, file_name, clip_num)
         clip.write_videofile(clip_path, audio=False)
-        target_key = '{}/{}/{}/{}/{}_clip_{}.mp4'.format(
-            TARGET_ROOT_FOLDER, user_id, album_id, CLIP_LENGTH_SECS, file_name, suffix)
+        target_key = '{}/{}/{}/{}_clip_{}.mp4'.format(
+            TARGET_ROOT_FOLDER, user_id, album_id, file_name, clip_num)
         print(target_key)
         s3_session.meta.client.upload_file(
             clip_path, TARGET_BUCKET, target_key)
-        outputs.append(target_key)
+
+        # save meta to json file
+        metaPath = '{}/{}_clip_{}.txt'.format(write_path, file_name, clip_num)
+        metaTargetKey = '{}/{}/{}/{}_clip_{}.txt'.format(
+            TARGET_META_FOLDER, user_id, album_id, file_name, clip_num)
+        clipMeta = {
+            "path": target_key,
+            "metaPath": metaTargetKey,
+            "fileName": file_name,
+            "number": clip_num,
+            "startSec": startClip,
+            "endSec": endClip,
+            "userId": user_id,
+            "uploadId": album_id,
+        }
+        with open(metaPath, 'w') as outfile:
+            json.dump(clipMeta, outfile)
+        s3_session.meta.client.upload_file(
+            metaPath, TARGET_BUCKET, metaTargetKey)
+        outputs.append(clipMeta)
 
     return {
         "statusCode": 200,
         "headers": {"content-type": "application/json"},
         "body":  {
+            "userId": user_id,
+            "uploadId": album_id,
             "bucket": TARGET_BUCKET,
             "outputs": outputs,
         },
